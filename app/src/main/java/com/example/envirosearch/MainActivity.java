36e0d1613c9d830a11a5931b0b4fc4374e5100a4
Marmik <marmikp19@gmail.com>
2021-10-16 13:05:53 -0600

    feat: use button to capture user input

--- app/src/main/java/com/example/envirosearch/MainActivity.java
+++ app/src/main/java/com/example/envirosearch/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.envirosearch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,7 +18,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        // Capturing user input
+        companyNameInput = (EditText)findViewById(R.id.companyNameInput);
+        searchButton = (Button)findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                companyName = companyNameInput.getText().toString();
+            }
+        }
+        );
+
+        // Send company name to googleUtil.java for search result processing
+        googleUtil.getSearchResults(companyName);
+
+
     }
 }
