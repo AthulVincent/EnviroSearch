78b2e88ffe0916cfca111a9b9d7239c35cf9f3fa
Yihoi Jung <yihoi0311@gmail.com>
2021-10-16 20:12:57 -0400

    update: displayResult file testing

--- app/src/main/java/com/example/envirosearch/displayResult.java
+++ app/src/main/java/com/example/envirosearch/displayResult.java
@@ -20,31 +20,44 @@ public class displayResult extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_display_result);
 
-        TextView hyper1= (TextView) findViewById(R.id.textView2);
-        hyper1.setMovementMethod(LinkMovementMethod.getInstance());
-
-        TextView hyper2= (TextView) findViewById(R.id.textView5);
-        hyper2.setMovementMethod(LinkMovementMethod.getInstance());
-
-        TextView hyper3= (TextView) findViewById(R.id.textView8);
-        hyper3.setMovementMethod(LinkMovementMethod.getInstance());
-
-        TextView hyper4= (TextView) findViewById(R.id.textView11);
-        hyper4.setMovementMethod(LinkMovementMethod.getInstance());
-
-        TextView hyper5= (TextView) findViewById(R.id.textView14);
-        hyper5.setMovementMethod(LinkMovementMethod.getInstance());
-
         // or do: for(int i =0, int j = 2; i < 5; i++, j=j+3)
+
+        int[] textviewids = {(R.id.textview2), (R.id.textview3), (R.id.textview4), (R.id.textview5), (R.id.textview6),
+                (R.id.textview7), (R.id.textview8), (R.id.textview9), (R.id.textview10), (R.id.textview11),
+                        (R.id.textview12), (R.id.textview13), (R.id.textview14), (R.id.textview15), (R.id.textview16),
+                (R.id.textview17), (R.id.textview18), (R.id.textview19), (R.id.textview20), (R.id.textview21)
+                        (R.id.textview22), (R.id.textview23), (R.id.textview24), (R.id.textview25), (R.id.textview26),
+                (R.id.textview27), (R.id.textview28), (R.id.textview29), (R.id.textview30), (R.id.textview31)};
+
+        int i = 0;
+
+        ArrayList<TextView> textviewarray = new ArrayList<TextView>();
+
+        for(; i <= 31; i++){
+            textviewarray.add((TextView)findViewById(textviewids[i]));
+        }
+
+        for(int j = 2; j <= 29; j = j+3){
+            textviewarray.get(j).setMovementMethod(LinkMovementMethod.getInstance());
+        }
+
+        Bundle extras = getIntent().getExtras();
+        ArrayList<searchResult> SR = null;
+        if (extras != null) {
+            SR = (ArrayList<searchResult>) getIntent().getSerializableExtra("key");
+        }
+
+        for(int j = 0; j < SR.size(); j++){
+            textviewarray.get(j*3).setText(SR.get(j).title);
+            textviewarray.get(j*3+1).setText(SR.get(j).URL);
+            textviewarray.get(j*3+2).setText(SR.get(j).Snippet);
+        }
+
     }
 
-    public displayResult(LinkedList<searchResult> results){
 
-    }
 
-    public static void main(String args[]){
-        //displayResult all_results = new displayResult();
-    }
 }
