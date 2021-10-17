6f791e67ab4743220e76ba07e64e17a5b3dbdf92
MohannadShehata <88001942+MohannadShehata@users.noreply.github.com>
2021-10-17 00:44:49 -0400

    Added user input verification
    Display red text warning in case of invalid input

--- app/src/main/java/com/example/envirosearch/MainActivity.java
+++ app/src/main/java/com/example/envirosearch/MainActivity.java
@@ -3,6 +3,16 @@ package com.example.envirosearch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+<<<<<<< Updated upstream
+=======
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.regex.*;
+>>>>>>> Stashed changes
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onChange(View view){
+        companyNameInput = (EditText)findViewById(R.id.companyNameInput);
+        companyName = companyNameInput.getText().toString();
+        Pattern p= Pattern.compile("([a-zA-Z]+([\\-][a-zA-Z]+)*)");
+        Matcher m=p.matcher(companyName);
+        TextView txtView = (TextView)findViewById(R.id.Error);
+        if (!m.matches())
+            txtView.setVisibility(View.INVISIBLE);
+        else
+            txtView.setVisibility(View.VISIBLE);
     }
 }
